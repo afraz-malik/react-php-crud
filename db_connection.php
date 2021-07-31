d8aef4fbab87d1be2de65a753b4326e96b9ca2e8
Afraz Malik <afrazmalik321@gmail.com>
2021-07-31 19:05:16 +0500

    changing db

--- db_connection.php
+++ db_connection.php
@@ -3,5 +3,6 @@
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
 // $con = mysqli_connect('localhost', 'root', '','faculty');
-$con = mysqli_connect('sql6.freemysqlhosting.net', 'sql6427280', 'jxgnA7HHAQ','sql6427280');
+// $con = mysqli_connect('sql6.freemysqlhosting.net', 'sql6427280', 'jxgnA7HHAQ','sql6427280');
+$con = mysqli_connect('sql6.freesqldatabase.com', 'sql6428551', 'eKEgv5GlgD','sql6428551');
 ?>
